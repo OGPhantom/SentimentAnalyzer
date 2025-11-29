6d0bc51f1937575cc9a992019ec5ea65864feb2d
OGPhantom <nikita.storchai@nure.ua>
2025-11-29 16:59:59 +0200

    Add implementation for previously created data models

--- SentimentAnalyzer/Models/Sentiment.swift
+++ SentimentAnalyzer/Models/Sentiment.swift
@@ -4,5 +4,43 @@
 //
 //  Created by Никита Сторчай on 28.11.2025.
 //
+import Charts
+import SwiftUI
 
-import Foundation
+enum Sentiment: String, Plottable {
+    case positive = "Positive"
+    case negative = "Negative"
+    case moderate = "Moderate"
+
+    init(_ score: Double) {
+        if score > 0.2 {
+            self = .positive
+        } else if score < -0.2 {
+            self = .negative
+        } else {
+            self = .moderate
+        }
+    }
+
+    var icon: String {
+        switch self {
+        case .positive:
+            return "chevron.up.2"
+        case .negative:
+            return "chevron.down.2"
+        case .moderate:
+            return "minus"
+        }
+    }
+
+    var sentimentColor: Color {
+        switch self {
+        case .positive:
+            return .teal
+        case .negative:
+            return .red
+        case .moderate:
+            return .gray
+        }
+    }
+}
